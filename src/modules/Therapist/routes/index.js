eb560729f628068ad6e7c52801f899b36e14d5d9
AlfredBryan <alfred.chimereze@gmail.com>
2019-04-15 20:34:37 +0100

    Add therapist routes

---
+++ src/modules/Therapist/routes/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const ctrlTherapist = require('../controllers');
+const {
+  catchErrors, verifyToken, checkTokenExists,
+} = require('../../../helpers');
+
+const router = express.Router();
+
+router.put('/edit/:Id', checkTokenExists, verifyToken, catchErrors(ctrlTherapist.updateTherapist));
+router.get('/profile/:id', catchErrors(ctrlTherapist.viewTherapist));
+router.get('/all', checkTokenExists, verifyToken, catchErrors(ctrlTherapist.allTherapists));
+
+module.exports = router;
