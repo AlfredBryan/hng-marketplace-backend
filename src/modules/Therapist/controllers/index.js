54fad3f4a9c0c28276396a42c1d0c60492a31428
Lord Jerry <lovinjerry004@gmail.com>
2019-04-15 22:44:14 +0100

    implent verifiing therapists

--- src/modules/Therapist/controllers/index.js
+++ src/modules/Therapist/controllers/index.js
@@ -1,8 +1,9 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const { sendJSONResponse } = require("../../../helpers");
+const { sendJSONResponse } = require('../../../helpers');
 
-const Therapist = mongoose.model("Therapist");
+const Therapist = mongoose.model('Therapist');
+const User = mongoose.model('User');
 
 module.exports.updateTherapist = async (req, res) => {
   const {
@@ -56,9 +57,9 @@ module.exports.updateTherapist = async (req, res) => {
       req.method,
       "Therapist Updated Succesfully!"
     );
-  } else {
+  } 
     return sendJSONResponse(res, 409, null, req.method, "Therapist not Found!");
-  }
+  
 };
 
 module.exports.allTherapists = async (req, res) => {
@@ -71,9 +72,9 @@ module.exports.allTherapists = async (req, res) => {
   const therapist = await Therapist.find({}, except);
 
   if (therapist) {
-    sendJSONResponse(res, 200, therapist, req.method, "All therapists");
+    sendJSONResponse(res, 200, therapist, req.method, 'All therapists');
   } else {
-    sendJSONResponse(res, 404, null, req.method, "No therapist  available");
+    sendJSONResponse(res, 404, null, req.method, 'No therapist  available');
   }
 };
 
@@ -81,13 +82,13 @@ module.exports.viewTherapist = async (req, res) => {
   const { id } = req.params;
 
   if (!id.match(/^[0-9a-fA-F]{24}$/)) {
-    return sendJSONResponse(res, 400, null, req.method, "Invalid Therapist ID");
+    return sendJSONResponse(res, 400, null, req.method, 'Invalid Therapist ID');
   }
 
   const therapist = await Therapist.findOne({ _id: id });
 
   if (therapist === null) {
-    return sendJSONResponse(res, 404, null, req.method, "Therapist Not Found");
+    return sendJSONResponse(res, 404, null, req.method, 'Therapist Not Found');
   }
 
   sendJSONResponse(
@@ -105,7 +106,7 @@ module.exports.viewTherapist = async (req, res) => {
       status: therapist.status,
     },
     req.method,
-    "View Therapist"
+    'View Therapist',
   );
 };
 // TODO: test if .find.populate works
@@ -120,13 +121,43 @@ module.exports.search = async (req, res) => {
     }
   }
 
-  const findTherapist = Therapist.findOne({ searchKey })
-    .populate({
-      path: 'User',
-      select: 'first_name last_name',
-    })
-    .exec();
+  const findTherapist = await Therapist.aggregate([
+    { $match: searchKey  },
+    { '$unwind': '$user' },
+    {
+      $lookup: {
+        from: 'User',
+        localField: 'user',
+        foreignField: '_id',
+        as: 'user',
+      },
+    },
+  ]).exec();
+  console.log(findTherapist);
 
   return sendJSONResponse(res, 200, { therapist: findTherapist }, req.method, 'Therapist fetched');
 };
 
+module.exports.create = async (req, res) => {
+  const { userId } = req.params;
+  const findUser = await User.findOne({ _id: userId });
+
+  if (!findUser) {
+    return sendJSONResponse(res, 200, {}, req.method, 'User not found');
+  }
+
+  if (findUser.designation.toLowerCase() !== 'therapist') {
+    return sendJSONResponse(res, 200, {}, req.method, 'User not a Therapist');
+  }
+
+  const findTherapist = await Therapist.findOne({ user: userId });
+  if (findTherapist) {
+    return sendJSONResponse(res, 200, {}, req.method, 'Account already approved');
+  }
+
+  const therapist = new Therapist();
+  therapist.user = findUser._id;
+  await therapist.save();
+
+  return sendJSONResponse(res, 201, {}, req.method, 'Therapist account approved');
+};
--- src/modules/Therapist/routes/index.js
+++ src/modules/Therapist/routes/index.js
@@ -5,10 +5,11 @@ const {
 } = require('../../../helpers');
 
 const router = express.Router();
-
+// TODO: remove all commented out authentication middleware
 router.put('/edit/:Id', checkTokenExists, verifyToken, catchErrors(ctrlTherapist.updateTherapist));
 router.get('/profile/:id', catchErrors(ctrlTherapist.viewTherapist));
-router.get('/all', checkTokenExists, verifyToken, catchErrors(ctrlTherapist.allTherapists));
+router.get('/all', /*checkTokenExists, verifyToken,*/ catchErrors(ctrlTherapist.allTherapists));
 router.post('/search', /*checkTokenExists, verifyToken,*/ catchErrors(ctrlTherapist.search));
+router.post('/verify/:userId', /*checkTokenExists, verifyToken,*/ catchErrors(ctrlTherapist.create));
 
 module.exports = router;
