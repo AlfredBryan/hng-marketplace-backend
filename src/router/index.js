93ce60812cbf54f28f34a9092cc0d19b35be287d
Lord Jerry <lovinjerry004@gmail.com>
2019-04-15 23:30:07 +0100

    --amend

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const userRoutes = require('../modules/User/routes');
 const requestRoutes = require('../modules/Request/routes');
-
+const therapistRoutes = require('../modules/Therapist/routes');
 const router = express.Router();
 
 router.use('/user', userRoutes);
 router.use(requestRoutes);
+router.use('/therapist', therapistRoutes);
 
 module.exports = router;
